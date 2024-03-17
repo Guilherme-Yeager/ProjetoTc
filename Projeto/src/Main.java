4fd8eb80acf2c56850b5deae1133f7c2d0058bab
Guilherme Lima <grguilh2rm2lima@gmail.com>
2024-03-17 01:51:01 -0300

    Estrutura do projeto.

--- Projeto/src/Main.java
+++ Projeto/src/Main.java
@@ -3,13 +3,6 @@ import utilities.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Arquivo arquivo = new Arquivo();
-        // arquivo.obterCaminho();
-        // ValidarAutomato validarAutomato = new ValidarAutomato();
-        // if (validarAutomato.isAfd(arquivo)) {
-        // System.out.println("é um AFD");
-        // } else {
-        // System.out.println("Não é um AFD");
-        // }
+
     }
 }
